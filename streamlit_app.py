89921b344fbb760bd0bcaf9fbfb456244bd3677b
Rohit Thakkar <97794321+Rohit-Thakkar@users.noreply.github.com>
2023-10-19 19:53:12 +0530

    changing it for my prject

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,63 @@
-from collections import namedtuple
+import streamlit as st
+import pandas as pd
+
+# Load your Titanic dataset here
+# Example: df = pd.read_csv('titanic_dataset.csv')
+
+st.title("Titanic Survival Prediction")
+
+# Display some basic information about the dataset
+st.write("This app provides insights into the Titanic dataset and allows you to make predictions.")
+st.write("The Titanic dataset contains information about passengers on the Titanic.")
+
+# Show the first few rows of the dataset
+if 'df' in locals():
+    st.write("Sample Data:")
+    st.write(df.head())
+
+# Sidebar for user input
+st.sidebar.header("User Input")
+
+# Allow the user to select a passenger by index
+passenger_index = st.sidebar.number_input("Select Passenger Index", min_value=0, max_value=len(df) - 1, value=0)
+
+# Display the details of the selected passenger
+if 'df' in locals():
+    st.sidebar.write("Selected Passenger:")
+    st.sidebar.write(df.iloc[passenger_index])
+
+# Create a section for data exploration
+st.header("Data Exploration")
+
+# Display a bar chart of passenger class distribution
+if 'df' in locals():
+    st.subheader("Passenger Class Distribution")
+    class_counts = df['Pclass'].value_counts()
+    st.bar_chart(class_counts)
+
+# Create a section for prediction
+st.header("Survival Prediction")
+
+# Show a form to input features for prediction (you can customize this based on your model's input features)
+if 'df' in locals():
+    st.subheader("Enter Passenger Information for Prediction")
+    name = st.text_input("Name")
+    age = st.number_input("Age", min_value=0)
+    gender = st.selectbox("Gender", ('Male', 'Female'))
+    pclass = st.selectbox("Passenger Class", (1, 2, 3))
+
+    # Add more features as needed for your model
+
+    # Create a button to make a prediction
+    if st.button("Predict Survival"):
+        # Make your prediction here using your machine learning model
+        # Replace this with your actual prediction code
+        prediction = "Survived"  # Replace with your model's prediction logic
+        st.write(f"Prediction: The passenger is likely to have {prediction}")
+
+
+
+"""from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
@@ -35,4 +94,4 @@ with st.echo(code_location='below'):
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        .encode(x='x:Q', y='y:Q'))"""
